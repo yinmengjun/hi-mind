fbe131f3c19fc15736b23695fae0b895eaf1c724
yinmengjun <51102691+yinmengjun@users.noreply.github.com>
2022-06-14 08:33:15 +0800

    Update cat.sh

--- misc/cat.sh
+++ misc/cat.sh
@@ -1,7 +1,7 @@
 cat *.sql
-# cat "mm"
-# line11,line12
-# line21,line22
+#cat "mm"
+#line11,line12
+#line21,line22
 lines=(`cat "mm"`)
 echo ${lines[@]}
-# line11,line12 line21,line22
+#line11,line12 line21,line22
