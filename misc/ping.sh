42e8fe421ed112b59d7db1ae3e39bd2f0bce2e30
yinmengjun <51102691+yinmengjun@users.noreply.github.com>
2022-06-20 03:21:20 +0800

    Update ping.sh

--- misc/ping.sh
+++ misc/ping.sh
@@ -1,9 +1,9 @@
 ping -c 3 1.1.1.1
-# PING 1.1.1.1 (1.1.1.1) 56(84) bytes of data.
-# 64 bytes from 1.1.1.1: icmp_seq=1 ttl=128 time=135 ms
-# 64 bytes from 1.1.1.1: icmp_seq=2 ttl=128 time=135 ms
-# 64 bytes from 1.1.1.1: icmp_seq=3 ttl=128 time=131 ms
-# 
-# --- 1.1.1.1 ping statistics ---
-# 3 packets transmitted, 3 received, 0% packet loss, time 2005ms
-# rtt min/avg/max/mdev = 130.562/133.813/135.482/2.299 ms
+#PING 1.1.1.1 (1.1.1.1) 56(84) bytes of data.
+#64 bytes from 1.1.1.1: icmp_seq=1 ttl=128 time=135 ms
+#64 bytes from 1.1.1.1: icmp_seq=2 ttl=128 time=135 ms
+#64 bytes from 1.1.1.1: icmp_seq=3 ttl=128 time=131 ms
+#
+#--- 1.1.1.1 ping statistics ---
+#3 packets transmitted, 3 received, 0% packet loss, time 2005ms
+#rtt min/avg/max/mdev = 130.562/133.813/135.482/2.299 ms
