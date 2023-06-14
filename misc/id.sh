466a94d6840d49b9a204cd754e00afd9f332c1c1
yinmengjun <51102691+yinmengjun@users.noreply.github.com>
2023-06-14 10:02:46 +0800

    Delete id.sh

--- misc/id.sh
+++
@@ -1,6 +0,0 @@
-id
-#uid=1000(hadoop) gid=1000(hadoop) groups=1000(hadoop),4(adm),24(cdrom),27(sudo),30(dip),46(plugdev),120(lpadmin),131(lxd),132(sambashare)
-id hadoop
-#uid=1000(hadoop) gid=1000(hadoop) groups=1000(hadoop),4(adm),24(cdrom),27(sudo),30(dip),46(plugdev),120(lpadmin),131(lxd),132(sambashare)
-id -un
-#hadoop
