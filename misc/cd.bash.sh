d0cd0fc05981625d8aa482e751901dc89177de88
yinmengjun <51102691+yinmengjun@users.noreply.github.com>
2023-09-16 16:59:40 +0800

    Update cd.bash.sh

--- misc/cd.bash.sh
+++ misc/cd.bash.sh
@@ -1,5 +1,5 @@
-cd
-cd ~
-cd ..
-cd -  #切换到上一个工作目录
+cd # goes to home directory
+cd ~ # change to home directory
+cd .. # go up one directory
+cd -  #切换到上一个工作目录 # change to last directory
 cd /databak/workspace
