5c5d65ba45ff09e9dd813fa5394cd84f017ef8e2
yinmengjun <51102691+yinmengjun@users.noreply.github.com>
2024-01-09 15:56:12 +0800

    Update uniq.bash.sh

--- misc/uniq.bash.sh
+++ misc/uniq.bash.sh
@@ -5,6 +5,7 @@
 #eee:50:5.5
 #bbb:20:2.2
 #eee:50:5.5
+
 uniq uniq.txt
 #aaa:10:1.1
 #ccc:30:3.3
@@ -12,6 +13,7 @@ uniq uniq.txt
 #eee:50:5.5
 #bbb:20:2.2
 #eee:50:5.5
+
 uniq -u uniq.txt
 #aaa:10:1.1
 #ccc:30:3.3
@@ -19,6 +21,7 @@ uniq -u uniq.txt
 #eee:50:5.5
 #bbb:20:2.2
 #eee:50:5.5
+
 sort uniq.txt
 #aaa:10:1.1
 #bbb:20:2.2
@@ -26,28 +29,33 @@ sort uniq.txt
 #ddd:40:4.4
 #eee:50:5.5
 #eee:50:5.5
+
 sort -u uniq.txt
 #aaa:10:1.1
 #bbb:20:2.2
 #ccc:30:3.3
 #ddd:40:4.4
 #eee:50:5.5
+
 sort uniq.txt | uniq
 #aaa:10:1.1
 #bbb:20:2.2
 #ccc:30:3.3
 #ddd:40:4.4
 #eee:50:5.5
+
 sort uniq.txt | uniq -u
 #aaa:10:1.1
 #bbb:20:2.2
 #ccc:30:3.3
 #ddd:40:4.4
+
 sort uniq.txt | uniq -c
 #      1 aaa:10:1.1
 #      1 bbb:20:2.2
 #      1 ccc:30:3.3
 #      1 ddd:40:4.4
 #      2 eee:50:5.5
+
 sort uniq.txt | uniq -d
 #eee:50:5.5
