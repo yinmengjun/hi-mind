226890ec624a3d84e79602a43c7c86c5ec8333a2
yinmengjun <51102691+yinmengjun@users.noreply.github.com>
2024-01-09 15:54:28 +0800

    Update tr.bash.sh

--- misc/tr.bash.sh
+++ misc/tr.bash.sh
@@ -1,6 +1,8 @@
 echo "HELLO WORLD" | tr 'A-Z' 'a-z'
 #hello world
+
 echo "hello 123 world 456" | tr -d '0-9'
-#hello  world 
+#hello  world
+
 echo "thissss is      a text linnnnnnne." | tr -s ' sn'
 #this is a text line.
